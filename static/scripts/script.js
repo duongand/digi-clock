2c9ec35f696b0a352904ec2d435f34f5781b5be3
duongand <duonganderson@gmail.com>
2022-06-29 21:11:57 -0400

    Time, date functionss; CSS styling

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,5 +1,3 @@
-console.log('Script loaded successfully');
-
 // Numerical association of days and months
 let days = new Map([
     [0, "Sunday"],
@@ -31,13 +29,31 @@ function check_time() {
     // Date object initialization
     let current_date = new Date();
 
-    // Current time properties
+    // Current time properties, check if any of the values are less than 10; otherwise no need to reformat
     let hour = (current_date.getHours() > 9 ? String(current_date.getHours()) : `0${current_date.getHours()}`);
     let minute = (current_date.getMinutes() > 9 ? String(current_date.getMinutes()) : `0${current_date.getMinutes()}`)
     let seconds = (current_date.getSeconds() > 9 ? String(current_date.getSeconds()) : `0${current_date.getSeconds()}`)
-    
+
     // Time formatting
     let formatted_time = `${hour}:${minute}:${seconds}`;
+
+    // Update clock DOM with change in time
+    let clock_display = document.getElementById('digital-clock');
+    clock_display.textContent = formatted_time;
+
+    // Update CSS stylesheet based on the time of the day
+    if (hour > 4 && hour < 16) {
+        document.getElementById('page-style').setAttribute('href', './static/css/morning.css')
+    } else {
+        document.getElementById('page-style').setAttribute('href', './static/css/night.css')
+    }
+
+};
+
+function check_date() {
+
+    // Date object initialization
+    let current_date = new Date();
 
     // Current date properties
     let day = current_date.getDay();
@@ -48,14 +64,12 @@ function check_time() {
     // Date formatting
     let formatted_date = `${days.get(day)}, ${months.get(month)} ${date} ${year}`;
 
-    // Update clock DOM with change in time
-    let clock_display = document.getElementById('digital-clock');
-    clock_display.textContent = formatted_time;
-
+    // Update date DOM
     let date_display = document.getElementById('date');
     date_display.textContent = formatted_date;
 
 };
 
 // Routinely check and change the time/date
-let date_timer = setInterval(check_time, 1000);
+let clock_timer = setInterval(check_time, 1000);
+let date_timer = setInterval(check_date, 1000);
