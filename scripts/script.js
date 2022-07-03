35dc58939586451aef315b784e56405224ea4e6e
duongand <duonganderson@gmail.com>
2022-07-03 16:34:25 -0400

    12 hour clock, date suffix, code cleanup

--- scripts/script.js
+++ scripts/script.js
@@ -1,75 +1,43 @@
-// Numerical association of days and months
-let days = new Map([
-    [0, "Sunday"],
-    [1, "Monday"],
-    [2, "Tuesday"],
-    [3, "Wednesday"],
-    [4, "Thursday"],
-    [5, "Friday"],
-    [6, "Saturday"]
-]);
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let numberSuffix = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'];
 
-let months = new Map([
-    [1, "January"],
-    [2, "February"],
-    [3, "March"],
-    [4, "April"],
-    [5, "May"],
-    [6, "June"],
-    [7, "July"],
-    [8, "August"],
-    [9, "September"],
-    [10, "October"],
-    [11, "November"],
-    [12, "December"]
-]);
+function checkTime() {
+    let currentDate = new Date();
 
-function check_time() {
+    let hour = currentDate.getHours();
+    let minute = checkZero(currentDate.getMinutes());
+    let seconds = checkZero(currentDate.getSeconds());
+    let timeOfDay;
 
-    // Date object initialization
-    let current_date = new Date();
+    if (hour < 13) {
+        timeOfDay = 'AM';
+    } else {
+        timeOfDay = 'PM';
+        hour = checkZero(hour -= 12);
+    };
 
-    // Current time properties, check if any of the values are less than 10; otherwise no need to reformat
-    let hour = (current_date.getHours() > 9 ? String(current_date.getHours()) : `0${current_date.getHours()}`);
-    let minute = (current_date.getMinutes() > 9 ? String(current_date.getMinutes()) : `0${current_date.getMinutes()}`)
-    let seconds = (current_date.getSeconds() > 9 ? String(current_date.getSeconds()) : `0${current_date.getSeconds()}`)
-
-    // Time formatting
-    let formatted_time = `${hour}:${minute}:${seconds}`;
-
-    // Update clock DOM with change in time
-    let clock_display = document.getElementById('digital-clock');
-    clock_display.textContent = formatted_time;
+    document.getElementById('digital-clock').textContent = `${hour}:${minute}:${seconds} ${timeOfDay}`;
 
     // Update CSS stylesheet based on the time of the day
-    if (hour > 4 && hour < 16) {
+    if (timeOfDay === 'AM') {
         document.getElementById('page-style').setAttribute('href', './styles/morning.css')
     } else {
         document.getElementById('page-style').setAttribute('href', './styles/night.css')
-    }
+    };
 
+    let day = currentDate.getDay();
+    let month = currentDate.getMonth();
+    let date = currentDate.getDate();
+    let year = currentDate.getFullYear();
+    let dateSuffix = numberSuffix[parseInt((date).toString().slice(-1))];
+
+    document.getElementById('date').textContent = `${days[day]}, ${months[month]} ${date}${dateSuffix} ${year}`;
 };
 
-function check_date() {
-
-    // Date object initialization
-    let current_date = new Date();
-
-    // Current date properties
-    let day = current_date.getDay();
-    let month = current_date.getMonth();
-    let date = current_date.getDate();
-    let year = current_date.getFullYear();
-
-    // Date formatting
-    let formatted_date = `${days.get(day)}, ${months.get(month)} ${date} ${year}`;
-
-    // Update date DOM
-    let date_display = document.getElementById('date');
-    date_display.textContent = formatted_date;
-
+function checkZero(number) {
+    return (number < 10 ? `0${number}` : number);
 };
 
-// Routinely check and change the time/date
-let clock_timer = setInterval(check_time, 1000);
-let date_timer = setInterval(check_date, 1000);
+checkTime();
+setInterval(checkTime, 1000);
